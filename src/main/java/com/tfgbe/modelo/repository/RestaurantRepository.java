5757971aa1b298e523f446e745b269e4c1834c16
Jesus Pastor <e3c.pastor25@gmail.com>
2025-11-25 12:03:21 +0100

    Add Restaurant, RestaurantRepository, RestaurantService, RestaurantServiceImpl, RestaurantRestController

---
+++ src/main/java/com/tfgbe/modelo/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.tfgbe.modelo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tfgbe.modelo.entities.Restaurant;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, String>{
+
+}
---
+++ src/main/java/com/tfgbe/modelo/services/RestaurantService.java
@@ -0,0 +1,7 @@
+package com.tfgbe.modelo.services;
+
+import com.tfgbe.modelo.entities.Restaurant;
+
+public interface RestaurantService extends ICrudGenerico<Restaurant, String>{
+	
+}
