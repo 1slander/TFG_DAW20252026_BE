6e1542dc439be75fca6f7024b02c06261d7de22a
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2025-11-25 11:40:21 +0100

    add: class UserRestController

--- src/main/java/com/tfgbe/restcontroller/UserRestController.java
+++ src/main/java/com/tfgbe/restcontroller/UserRestController.java
@@ -1,9 +1,17 @@
 package com.tfgbe.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.tfgbe.modelo.services.UserService;
 
+
+
+@RestController
+@RequestMapping("/user")
 public class UserRestController {
+	@Autowired
+	UserService userService;
 
 }
