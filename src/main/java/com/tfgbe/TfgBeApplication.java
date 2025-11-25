512cc4d9220ff4d0eb3092dbd9c7c74184527ae9
1slander <hectorgomendez@gmail.com>
2025-11-25 08:40:27 +0000

    Primer commit

---
+++ src/main/java/com/tfgbe/TfgBeApplication.java
@@ -0,0 +1,13 @@
+package com.tfgbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfgbe/TfgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfgbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
